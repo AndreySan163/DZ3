cfeb1b9483142427670942b398c8729f7162890c
Andrei <nestorivanov163@gmail.com>
2023-10-20 18:06:40 +0400

    DZ33

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое целое число: ");
+        int one = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Введите второе целое число: ");
+        int two = scanner.nextInt();
+        scanner.nextLine();
+        int rez = one + two;
+        int rez1 = one - two;
+        int rez2 = one * two;
+        int rez3 = one / two;
+        System.out.printf("Сложение: %d \nВычетание: %d \nУмножение: %d \nДеление: %d  ", rez, rez1, rez2, rez3);
     }
 }
